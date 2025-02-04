7275a4ef5445987443c6faeaf6d96c0f8698f4aa
Jan Scheible <janScheible@users.noreply.github.com>
2025-02-04 22:47:37 +0100

    code cleanup

--- game/src/main/java/com/scheible/risingempire/game/impl/fleet/FleetIdGenerator.java
+++ game/src/main/java/com/scheible/risingempire/game/impl/fleet/FleetIdGenerator.java
@@ -1,9 +1,10 @@
 package com.scheible.risingempire.game.impl.fleet;
 
-import java.util.Collections;
 import java.util.Set;
 
 import com.scheible.risingempire.game.api.view.fleet.FleetId;
+
+import static java.util.Collections.unmodifiableSet;
 
 /**
  * @author sj
@@ -13,7 +14,7 @@ public class FleetIdGenerator {
 	private final Set<FleetId> used;
 
 	public FleetIdGenerator(Set<FleetId> used) {
-		this.used = Collections.unmodifiableSet(used);
+		this.used = unmodifiableSet(used);
 	}
 
 	public FleetId createRandom() {
