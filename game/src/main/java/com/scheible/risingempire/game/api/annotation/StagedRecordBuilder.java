e7c1b2a151a9f2385b7a2d212bee7cc14b41c40b
Jan Scheible <janScheible@users.noreply.github.com>
2026-02-08 15:40:23 +0100

    updated almost all dependencies (except Spring Boot 4)

--- game/src/main/java/com/scheible/risingempire/game/api/annotation/StagedRecordBuilder.java
+++ game/src/main/java/com/scheible/risingempire/game/api/annotation/StagedRecordBuilder.java
@@ -8,13 +8,14 @@ import java.lang.annotation.Target;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 import io.soabase.recordbuilder.core.RecordBuilder.BuilderMode;
+import io.soabase.recordbuilder.core.RecordBuilder.ConcreteSettersForOptionalMode;
 
 /**
  * @author sj
  */
 @RecordBuilder.Template(options = @RecordBuilder.Options(builderMode = BuilderMode.STAGED_REQUIRED_ONLY,
-		nullablePattern = "^((Initialized))$", addConcreteSettersForOptional = true, emptyDefaultForOptional = false,
-		addClassRetainedGenerated = true))
+		nullablePattern = "^((Initialized))$", addConcreteSettersForOptional = ConcreteSettersForOptionalMode.ENABLED,
+		emptyDefaultForOptional = false, addClassRetainedGenerated = true))
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 @Inherited
